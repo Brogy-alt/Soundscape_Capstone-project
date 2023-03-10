ec52b923fa35f5dd314171a1e706a5cf0293ef3e
Brogy-alt <brogangys2@gmail.com>
2023-03-10 16:40:19 +0200

    kinda did landingpage

---
+++ frontend/src/components/FooterC.vue
@@ -0,0 +1,101 @@
+<template>
+    <div>
+        <footer>
+            <div class="footer">
+                <div class="row">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+
+                <div class="row">
+                    <ul>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Our Services</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Career</a></li>
+                    </ul>
+                </div>
+
+                <div class=" m-0 pb-2" id="copyright">
+                    
+                    Virtuverse Copyright © 2023 Virtuverse - All rights reserved || Designed By: Brogan Gys
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+/* .footer{
+  font-size: larger;
+} */
+.footer {
+    background: #ffffff71;
+    padding: 40px 0px;
+    font-family: 'Play', sans-serif;
+    text-align: center;
+}
+
+.footer #copyright {
+    width: 100%;
+    margin: 0px 10px;
+    padding: 0.6% 0%;
+    color: rgb(0, 0, 0);
+    font-size: 1rem;
+    
+    
+    
+}
+
+.footer .row a {
+    text-decoration: none;
+    color: rgb(0, 0, 0);
+    transition: 0.5s;
+}
+
+.footer .row a:hover {
+    color: #0800007a;
+}
+
+.footer .row ul {
+    width: 100%;
+}
+
+.footer .row ul li {
+    display: inline-block;
+    margin: 0px 30px;
+}
+
+.footer .row a i {
+    font-size: 2em;
+    margin: 0% 1%;
+}
+
+@media (max-width:720px) {
+    .footer {
+        text-align: left;
+        padding: 5%;
+    }
+
+    .footer .row ul li {
+        display: block;
+        margin: 10px 0px;
+        text-align: left;
+    }
+
+    .footer .row a i {
+        margin: 0% 3%;
+    }
+}
+</style>      
+
+
---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <NavbarC/>
+    <div>
+        <section id="home">
+        <div class="container-fluid">
+        <div class="row d-flex flex-column flex-md-row">
+            <div loading="lazy" class="img1">
+                <div class="home-text col">
+                    <!-- <h1 id="heading-homepage" class="lead">The New Wave Of Sound Ready For Your Use</h1> -->
+                    <!-- <p class="home-p-text col">Welcome to my beat store! This was created for you are the writer, the performer, the beat maker, the producer, the engineer, the record label, the publisher, the videographer, the artist and the CREATOR. Music is the universal language that connects us across the globe. Music brings out our human emotions and music is the unified voice of the people and 
+                    this site allows anyone to get all my creations and to hopefully work together in the future.
+                    </p> -->
+
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+    
+</div>
+<FooterC/>
+</template>
+<script>
+
+import NavbarC from '@/components/NavbarC.vue';
+import FooterC from '@/components/FooterC.vue';
+
+export default {
+    components: {
+        NavbarC,
+        FooterC
+    }
+}
+</script>
+<style scoped>
+
+/* landing page img */
+.img1 {
+    width: 100vw;
+    height: 100vh;
+    background: url(https://i.postimg.cc/ZKgNT8tz/landing-1.jpgy);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center 
+    
+}
+
+/* landing page text */
+#heading-homepage {
+    font-family:Georgia, 'Times New Roman', Times, serif;
+    font-size: 72px;
+    color: #FDF7FF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+ 
+}
+    
+</style>
